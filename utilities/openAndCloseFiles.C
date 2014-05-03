3fc1bc59bdda3d079ef4159452c5152d49cdc825
Ryan Nichol <r.nichol@ucl.ac.uk>
2014-05-03 10:44:14 +0100

    Adding macro to open and close all prediciton files

---
+++ utilities/openAndCloseFiles.C
@@ -0,0 +1,19 @@
+
+
+
+void openAndCloseFiles()
+{
+
+  const char *fileTypeTag[3]={"main","nue","tau"};
+
+  char fileName[FILENAME_MAX];
+  for(int fileType=0;fileType<3;fileType++) {
+    for(int dmi=0;dmi<1;dmi++) {
+      sprintf(fileName,"farPreds/run11/%s%d.root",fileTypeTag[fileType],dmi);
+      TFile *fp = TFile::Open(fileName,"UPDATE");
+      fp->Close();
+    }
+  }
+}
+
+
